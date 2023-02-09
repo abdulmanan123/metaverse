1121126552d311602603a0bc8cabaecff4173d77
Abdul Manan <abdulmanan@pitb.gov.pk>
2023-02-09 16:37:24 +0500

    first commit

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -15,10 +15,10 @@ const Navbar = () => (
 
     <div className="absolute w-[50%] inset-0 gradient-01" />
 
-    <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`} >
+    <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
       <img src="/search.svg" className="w-[24px] h-[24px] object-contain" />
       <h2 className="font-extrabold text-[24px] leading-[30px] text-white">
-        METAVERSUS
+        METAVERSE
       </h2>
       <img src="/menu.svg" className="w-[24px] h-[24px] object-contain" />
     </div>
